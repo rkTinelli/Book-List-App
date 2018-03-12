9be29cd6b44f7a6fbf7a232072a77e57852b3be8
Henrique Tinelli <rktinelli@gmail.com>
2018-03-12 16:28:56 -0300

    Initial Files
    Initial Files for the project with the basic List View Activity

---
+++ app/src/main/java/br/com/rktin/booklist/BookList.java
@@ -0,0 +1,13 @@
+package br.com.rktin.booklist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+    }
+}
